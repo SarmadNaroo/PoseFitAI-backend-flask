b7ef90d6e746af17260666959a69fb84069bb53f
Sarmad <sarmadnaroo.bsiietkhpf20@iba-suk.edu.pk>
2024-05-08 03:45:06 +0500

    get all users route jwt required added

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, jsonify
 from app.models.user import User
 from app.models import db
+from flask_jwt_extended import jwt_required, current_user, get_jwt_identity
 
-users_blueprint = Blueprint('users', __name__)
+users_blueprint = Blueprint('user', __name__)
 
-@users_blueprint.route('/', methods=['GET'])
+@users_blueprint.route('/all', methods=['GET'])
+@jwt_required()
 def get_users():
     users = User.query.all()
     users_list = [{
@@ -18,3 +20,18 @@ def get_users():
     } for user in users]
 
     return jsonify(users_list), 200
+
+@users_blueprint.route('/profile', methods=['GET'])
+@jwt_required()
+def get_profile():
+    id = current_user.id
+    user = User.query.get(id)
+    return jsonify({
+        'id': user.id,
+        'username': user.username,
+        'email': user.email,
+        'weight': user.weight,
+        'height': user.height,
+        'gender': user.gender,
+        'age': user.age,
+    }), 200
